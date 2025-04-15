09a581b72dba5fa46f1efe92ee19c6fc1279dcd0
Anand2027 <anandgupta020204@gmail.com>
2025-04-15 10:48:04 +0530

    code

--- src/components/Landing02.jsx
+++ src/components/Landing02.jsx
@@ -5,19 +5,7 @@ const Landing02 = () => {
     <div className='max-w-[93vw] m-auto'>
       <p className='text-lg font-[550]'>My Skills</p>
       <p className='text-3xl font-bold'>My Expertise</p>
-      {/* <div className="bahar_dabbe">
-       <div className="dabbe bg-pink-400 h-50 flex flex-wrap gap-5 p-5">
       
-           
-           <div className="dabba bg-orange-300 mt-10 w-[calc(25%-20px)] h-30"></div>
-           
-           <div className="dabba bg-orange-300 mt-10 w-[calc(25%-20px)] h-30"></div>
-           
-           <div className="dabba bg-orange-300 mt-10 w-[calc(25%-20px)] h-30"></div>
-           
-           <div className="dabba bg-orange-300 mt-10 w-[calc(25%-20px)] h-30"></div>
-        </div>
-      </div> */}
 
     </div>
   )
