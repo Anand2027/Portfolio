28c3f517de65ea1150d9f130aaa5c3cacabebbb1
Anand2027 <anandgupta020204@gmail.com>
2025-05-31 18:04:29 +0530

    button-adjust

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -6,16 +6,17 @@ const WhatsAppButton = () => {
   const message = "Hello Anand! I saw your portfolio and would like to connect.";
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
-  return (
+return (
     <a
-      href={whatsappURL}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-5 right-20 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
+        href={whatsappURL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-18 right-5.5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300
+            sm:bottom-5 sm:right-18"
     >
-      <FaWhatsapp size={24} />
+        <FaWhatsapp size={24} />
     </a>
-  );
+);
 };
 
 export default WhatsAppButton;
